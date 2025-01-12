eeccb1493c16b8823bfa738e84ae6ebc6d0c71a6
ziy <1936893077@qq.com>
2025-01-12 11:29:04 +0800

    feat: 优化初始化脚本，增加git创建逻辑

--- initialize.sh
+++ initialize.sh
@@ -5,7 +5,18 @@ if [[ $confirm != [yY] ]]; then
   exit 0;
 fi
 
+read -p "是否创建代码仓库?(y/n): " confirm
+
+if [[ $confirm == [yY] ]]; then
+
+  read -p "请输入代码仓库地址: " repoUrl
+  rm -rf .git && git init
+  git remote add origin $repoUrl
+fi
+
+
 ./gradlew cleanIdea
 # 运行必要的初始化
 ./gradlew baselineUpdateConfig
 ./gradlew idea
+./gradlew dependencies
