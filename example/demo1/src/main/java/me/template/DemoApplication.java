5ccfc4e594b6081a7faaad9f689fe9c5e2f72e7e
ziy <1936893077@qq.com>
2025-01-19 13:01:30 +0800

    chore: 调整项目结构

--- example/demo1/src/main/java/me/template/DemoApplication.java
+++ example/demo1/src/main/java/me/template/DemoApplication.java
@@ -1,14 +1,26 @@
 package me.template;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- *
+ * demo1.
  * @author ziy
  * @date 2025-01
  */
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/demo1")
+    public ResponseEntity<String> demo1() {
+        return ResponseEntity.ok("demo1");
+    }
 }
