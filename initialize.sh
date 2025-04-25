37a276c4a3e5681fe90763ea6abd716990c361c8
ziy <1936893077@qq.com>
2025-04-25 13:01:35 +0800

    优化初始化

--- initialize.sh
+++ initialize.sh
@@ -5,6 +5,10 @@ if [[ $confirm != [yY] ]]; then
   exit 0;
 fi
 
+read -p "group: " group
+export GROUP=$group
+envsubst < project/init/build.gradle.template > build.gradle
+
 read -p "是否创建代码仓库?(y/n): " confirm
 
 if [[ $confirm == [yY] ]]; then
@@ -14,4 +18,12 @@ if [[ $confirm == [yY] ]]; then
 fi
 
 
-./gradlew cleanIdea baselineUpdateConfig idea dependencies
+if ! ./gradlew cleanIdea baselineUpdateConfig idea dependencies; then
+  echo "gradle 初始化失败" >&2
+  exit 1
+fi
+
+# 清空999_palantir.txt文件 如果存在
+if [ -f ".baseline/copyright/999_palantir.txt" ]; then
+  echo "" > .baseline/copyright/999_palantir.txt
+fi
